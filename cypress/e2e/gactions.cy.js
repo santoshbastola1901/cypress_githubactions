9ca514691d580b419b95935c38f6a100edc1ea48
Santosh Bastola <santosh.bastola@bajratechnologies.com>
2025-12-18 17:19:38 +0000

    feat: make corrections

--- cypress/e2e/gactions.cy.js
+++ cypress/e2e/gactions.cy.js
@@ -7,7 +7,6 @@ describe("Bajra Login", () => {
 
     cy.get('button[type="submit"]').click();
 
-    // success assertion
     cy.get("#Inbox", { timeout: 10000 }).should("be.visible");
   });
 
@@ -19,29 +18,6 @@ describe("Bajra Login", () => {
 
     cy.get('button[type="submit"]').click();
 
-    // failure assertion
-    cy.contains("Invalid login").should("be.visible");
-  });
-
-  it("verify invalid-username-bajra-login", () => {
-    cy.visit("https://bajratechnologies.com/web/login");
-
-    cy.get("#login").type(Cypress.env("invalidusername"));
-    cy.get("#password").type(Cypress.env("validpassword"));
-
-    cy.get('button[type="submit"]').click();
-
-    cy.contains("Invalid login").should("be.visible");
-  });
-
-  it("verify invalid-password-bajra-login", () => {
-    cy.visit("https://bajratechnologies.com/web/login");
-
-    cy.get("#login").type(Cypress.env("validusername"));
-    cy.get("#password").type(Cypress.env("invalidpassword"));
-
-    cy.get('button[type="submit"]').click();
-
-    cy.contains("Invalid login").should("be.visible");
+    cy.contains(/invalid/i).should("be.visible");
   });
 });
