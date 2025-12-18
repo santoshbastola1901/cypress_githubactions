c0146b389bcff91fb40bbc1bc0cf006b77c45f40
Santosh Bastola <santosh.bastola@bajratechnologies.com>
2025-12-18 16:31:37 +0000

    feat: add new spec file and delete previous ones

---
+++ cypress/e2e/gactions.cy.js
@@ -0,0 +1,45 @@
+describe("Bajra Login", () => {
+  it("verify valid-bajra-login", () => {
+    cy.visit("https://bajratechnologies.com/web/login");
+
+    cy.get("#login").type(Cypress.env("validusername"));
+    cy.get("#password").type(Cypress.env("validpassword"));
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+    cy.contains("#Inbox");
+  });
+
+  it("verify invalid-bajra-login", () => {
+    cy.visit("https://bajratechnologies.com/web/login");
+
+    cy.get("#login").type(Cypress.env("invalidusername"));
+    cy.get("#password").type(Cypress.env("invalidpassword"));
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+    cy.contains("#Inbox");
+  });
+
+  it("verify invalid-username-bajra-login", () => {
+    cy.visit("https://bajratechnologies.com/web/login");
+
+    cy.get("#login").type(Cypress.env("invalidusername"));
+    cy.get("#password").type(Cypress.env("validpassword"));
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+    cy.contains("#Inbox");
+  });
+
+  it("verify invalid-password-bajra-login", () => {
+    cy.visit("https://bajratechnologies.com/web/login");
+
+    cy.get("#login").type(Cypress.env("validusername"));
+    cy.get("#password").type(Cypress.env("invalidpassword"));
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+    cy.contains("#Inbox");
+  });
+});
